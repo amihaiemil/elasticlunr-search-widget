16f39dbebab0b6e1550a33fafd610427b3f5d2d1
Mihai <amihaiemil@gmail.com>
2020-02-11 00:16:55 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,20 +12,16 @@
 set -e
 set -o pipefail
 
-#TOKEN=$(cat /home/r/deployment.txt)
-
-echo "<<<<<< RULTOR CONTAINER >>>>>>"
-echo $container
-echo "<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>"
+TOKEN=$(cat /home/r/deployment.txt)
 
 # deploy the build elasticlunr-search-widget.min.js
-#SHA_BUILD=$(curl 'https://api.github.com/repos/amihaiemil/amihaiemil.github.io/contents/js/elasticlunr/elasticlunr-search-widget.min.js' | jq '.sha')
-#NEW_BUILD=$(openssl enc -base64 <<< $(cat src/elasticlunr-search-widget.min.js) | awk 'BEGIN{ORS="\\n";} {print}')
-#echo "{\"message\": \"deploy new build\", \"sha\": ${SHA_BUILD}, \"content\": \"${NEW_BUILD}\"}" > build.txt;
-#curl -H "Authorization: token ${TOKEN}" -X PUT -d @build.txt https://api.github.com/repos/amihaiemil/amihaiemil.github.io/contents/js/elasticlunr/elasticlunr-search-widget.min.js
+SHA_BUILD=$(curl 'https://api.github.com/repos/amihaiemil/amihaiemil.github.io/contents/js/elasticlunr/elasticlunr-search-widget.min.js' | jq '.sha')
+NEW_BUILD=$(openssl enc -base64 <<< $(cat src/elasticlunr-search-widget.min.js) | awk 'BEGIN{ORS="\\n";} {print}')
+echo "{\"message\": \"deploy new build\", \"sha\": ${SHA_BUILD}, \"content\": \"${NEW_BUILD}\"}" > build.txt;
+curl -H "Authorization: token ${TOKEN}" -X PUT -d @build.txt https://api.github.com/repos/amihaiemil/amihaiemil.github.io/contents/js/elasticlunr/elasticlunr-search-widget.min.js
 
 # deploy the css file elasticlunr-search-widget_light.css
-#SHA_CSS_LIGHT=$(curl 'https://api.github.com/repos/amihaiemil/amihaiemil.github.io/contents/css/elasticlunr/elasticlunr-search-widget_light.css' | jq '.sha')
-#NEW_LIGHT_CSS=$(openssl enc -base64 <<< $(cat src/css/elasticlunr-search-widget_light.css) | awk 'BEGIN{ORS="\\n";} {print}')
-#echo "{\"message\": \"deploy css light\", \"sha\": ${SHA_CSS_LIGHT}, \"content\": \"${NEW_LIGHT_CSS}\"}" > css_light.txt
-#curl -H "Authorization: token ${TOKEN}" -X PUT -d @css_light.txt https://api.github.com/repos/amihaiemil/amihaiemil.github.io/contents/css/elasticlunr/elasticlunr-search-widget_light.css
+SHA_CSS_LIGHT=$(curl 'https://api.github.com/repos/amihaiemil/amihaiemil.github.io/contents/css/elasticlunr/elasticlunr-search-widget_light.css' | jq '.sha')
+NEW_LIGHT_CSS=$(openssl enc -base64 <<< $(cat src/css/elasticlunr-search-widget_light.css) | awk 'BEGIN{ORS="\\n";} {print}')
+echo "{\"message\": \"deploy css light\", \"sha\": ${SHA_CSS_LIGHT}, \"content\": \"${NEW_LIGHT_CSS}\"}" > css_light.txt
+curl -H "Authorization: token ${TOKEN}" -X PUT -d @css_light.txt https://api.github.com/repos/amihaiemil/amihaiemil.github.io/contents/css/elasticlunr/elasticlunr-search-widget_light.css
